bcd103b2a5de0fe0344d504bd0ab3f855c393869
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2018-09-14 23:42:14 -0300

    First implementation of dib3 and tdik

--- diacumulado.py
+++ diacumulado.py
@@ -2,18 +2,42 @@
 
 # http://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/metodologia-do-di.htm
 
-from typing import List
-from decimal import Decimal
+import decimal
 import unittest
+
+from decimal import Decimal
+from typing import List
 
 
 def dib3(taxas: List[Decimal], p: Decimal) -> Decimal:
-    return Decimal(0)
+    if not taxas:
+        return Decimal(0)
+
+    ret = Decimal(1)
+    for taxa in taxas:
+        di = tdik(taxa)
+        c = Decimal(1) + di * (p / Decimal(100))
+        ret *= c
+    return ret
 
 
-def tdik(di: Decimal):
-    return Decimal(0)
+def tdik(di: Decimal) -> Decimal:
+    decimal.getcontext().rounding = decimal.ROUND_FLOOR
+    ret = (di / Decimal(100) + 1)**(Decimal(1) / Decimal(252)) - 1
+    return round(ret, 8)
+
 
 class TestDI(unittest.TestCase):
 
-    pass
+    def test_tdik_one_day(self):
+        ret = tdik(Decimal('6.89'))
+        self.assertEqual(ret, Decimal('0.00026444'))
+
+    def test_dib3_zero_days(self):
+        ret = dib3([], Decimal('100.0000'))
+        self.assertEqual(ret, Decimal(0))
+        
+    def test_dib3_one_day(self):
+        ret = dib3([Decimal('6.89')], Decimal('100.0000'))
+        self.assertEqual(ret, Decimal('1.00026444'))
+
