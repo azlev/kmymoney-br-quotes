e7e5eda37669264fe2f6d57105d8958f116862df
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2018-09-14 23:14:54 -0300

    Basic functions with signatures

---
+++ diacumulado.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# http://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/metodologia-do-di.htm
+
+from typing import List
+from decimal import Decimal
+import unittest
+
+
+def dib3(taxas: List[Decimal], p: Decimal) -> Decimal:
+    return Decimal(0)
+
+
+def tdik(di: Decimal):
+    return Decimal(0)
+
+class TestDI(unittest.TestCase):
+
+    pass
